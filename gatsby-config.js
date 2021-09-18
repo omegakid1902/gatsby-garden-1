03f98ba624e068962f5a00015597f452b2af361c
omegakid1902 <anhdungbk09@gmail.com>
2021-09-18 21:13:29 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,21 +8,21 @@ module.exports = {
     title: `Gatsby Garden`,
     description: `A Digital Garden Tended by Gatsby`,
 
-    // siteUrl: `https://yoursite.com/notes/`, // URL at which your site will be published. This should be present if you want RSS feed.
-    // headerMenu: [ // Top Navbar items
-    //   {type: 'page', item: '', title: 'Home'}, // Type can be 'page', 'note', 'tag', or 'link'
-    //   {type: 'page', item: 'sitemap', title: 'Sitemap'},
-    //   {type: 'page', item: 'rss.xml', title: 'RSS'},
-    //   {
-    //     type: 'page', item: 'tags', title: 'Tags',
-    //     menu: [ // Only one level depth, please.
-    //       {type: 'tag',item: 'zettelkasten'},
-    //       {type: 'tag',item: 'philosophy'},
-    //       {type: 'tag',item: 'psychology'},
-    //       {type: 'tag',item: 'rationality'},
-    //     ]
-    //   },
-    // ],
+    siteUrl: `https://evergreen-garden.netlify.app/notes/`, // URL at which your site will be published. This should be present if you want RSS feed.
+    headerMenu: [ // Top Navbar items
+      {type: 'page', item: '', title: 'Home'}, // Type can be 'page', 'note', 'tag', or 'link'
+      {type: 'page', item: 'sitemap', title: 'Sitemap'},
+      {type: 'page', item: 'rss.xml', title: 'RSS'},
+      {
+        type: 'page', item: 'tags', title: 'Tags',
+        menu: [ // Only one level depth, please.
+          {type: 'tag',item: 'zettelkasten'},
+          {type: 'tag',item: 'philosophy'},
+          {type: 'tag',item: 'psychology'},
+          {type: 'tag',item: 'rationality'},
+        ]
+      },
+    ],
 
     // menu: [ // This is the Table of Contents that comes in the home page if a Home Note is not specified. It can be much longer than the header menu.
     //   ... Same structure as headerMenu. You can have any depth level - multiple menus can be nested.
