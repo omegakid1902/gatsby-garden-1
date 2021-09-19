2810a4bb66f9b6cf497d21ecd1b32162bb3d45b4
omegakid1902 <anhdungbk09@gmail.com>
2021-09-19 20:40:20 +0700

    Update publish.py

--- publish.py
+++ publish.py
@@ -3,6 +3,9 @@ import frontmatter
 from shutil import copy
 dirname = os.getcwd()
 
+if not os.path.exists('./_notes'):
+    os.mkdir('./_notes')
+    
 for file in os.listdir("../zettelkasten/"):
     if file.endswith(".md"):
         with open(os.path.join("../zettelkasten/", file), encoding="utf8") as f:
