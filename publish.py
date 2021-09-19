ecad6407ddab29279132faa9c702032573fb0684
omegakid1902 <anhdungbk09@gmail.com>
2021-09-19 20:12:04 +0700

    Update publish.py

--- publish.py
+++ publish.py
@@ -2,6 +2,34 @@ import os
 import frontmatter
 from shutil import copy
 import os
+import os
+import frontmatter
+from shutil import copy
+import os
+
+for file in os.listdir("../zettelkasten/"):
+    if file.endswith(".md"):
+        with open(os.path.join("../zettelkasten/", file), encoding="utf8") as f:
+            content = f.read()
+            metadata, content = frontmatter.parse(content)
+            if 'publish' in metadata.keys():
+                print("Copy publish files from zettelkasten to content/")
+                copy(os.path.join("../zettelkasten/", file), './_notes/')
+            else:
+                pass
+
+for root, dirs, files in os.walk("../zettelkasten/Zet/"):
+    for file in files:
+        if file.endswith(".md"):
+            with open(os.path.join(root, file), encoding="utf8") as f:
+                content = f.read()
+                metadata, content = frontmatter.parse(content)
+                if 'publish' in metadata.keys():
+                    print("Copy publish files from zettelkasten to content/")
+                    print(os.path.join(root, file))
+                    copy(os.path.join(root, file), './_notes/')
+                else:
+                    pass
 
 for root, dirs, files in os.walk("./_notes/"):
     for file in files:
